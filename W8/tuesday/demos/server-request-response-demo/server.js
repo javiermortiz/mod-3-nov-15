7f3518fe91815e051633daa8212d3129fd610116
Javier Ortiz <javier.ortiz989@gmail.com>
2022-01-11 09:08:15 -0800

    tuesday demo

--- W8/tuesday/demos/server-request-response-demo/server.js
+++ W8/tuesday/demos/server-request-response-demo/server.js
@@ -1,55 +1,15 @@
 const http = require('http');
 const fs = require("fs");
 
-let database = [];
 const server = http.createServer((req, res) => {
     console.log(`${req.method} ${req.url}`);
-        
+
     if (req.method === "GET" && req.url === "/") {
-        const htmlPage = fs.readFileSync("index.html", "utf-8");
-        const tasksList = database.map(task => {
-            return `<li>${task["tasks"]} - ${task["time"]}</li>`
-        });
-        const resBody = htmlPage
-            .replace(/#{tasks}/g, tasksList.join(""));
-        return res.end(resBody);
-    }
-
-    if (req.method === "GET" && req.url === "/main.css") {
-        const resBody = fs.readFileSync("main.css");
+        const resBody = fs.readFileSync("index.html");
         res.statusCode = 200;
-        res.setHeader("Content-Type", "text/css");
+        res.setHeader("Content-Type", "text/html");
         return res.end(resBody);
     }
-
-    let reqBody = "";
-    req.on("data", (data) => {
-        reqBody += data;
-    });
-
-    req.on("end", () => {
-        // Parsing the body of the request
-        if (reqBody) {
-            req.body = reqBody
-                .split("&")
-                .map((keyValuePair) => keyValuePair.split("="))
-                .map(([key, value]) => [key, value.replace(/\+/g, " ")])
-                .map(([key, value]) => [key, decodeURIComponent(value)])
-                .reduce((acc, [key, value]) => {
-                    acc[key] = value;
-                    return acc;
-                }, {});
-            console.log(req.body);
-        }
-        if (req.method === "POST" && req.url === "/tasks") {
-            console.log(req.body);
-            database.push(req.body);
-            res.statusCode = 302;
-            res.setHeader("Location", "/");
-            return res.end();
-        }
-    });
-    
 });
 
 const port = 5000;
