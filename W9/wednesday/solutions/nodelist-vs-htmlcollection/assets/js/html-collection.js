8fb933efea7b716a6c518507821136edc3033d9a
Javier Ortiz <javier.ortiz989@gmail.com>
2022-01-19 10:04:51 -0800

    selection

--- W9/wednesday/solutions/nodelist-vs-htmlcollection/assets/js/html-collection.js
+++ W9/wednesday/solutions/nodelist-vs-htmlcollection/assets/js/html-collection.js
@@ -1,5 +1,5 @@
 export default () => {
-  const bodyChildElements = document.body.children; // HTMLCollection [div]
+  const bodyChildElements = Array.from(document.body.children); // HTMLCollection [div]
   // console.log(bodyChildElements);
 
   const div = bodyChildElements[0];
@@ -7,5 +7,5 @@ export default () => {
   const divChildElements = div.children; // HTMLCollection [span]
   const helloWorld = div.innerText; // Hello World! Yes!    <-- NOT Hello World!
   const span = divChildElements[0]; // <span>Yes!</span>
-  debugger
+  // debugger
 };
